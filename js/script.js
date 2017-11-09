3f624d4cff0863c285a701d0a8ca8705ea7dc69c
Fisher <truefisherking@gmail.com>
2017-11-09 16:52:08 -0500

    Added PeopleConstructor class

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 var button = document.querySelector('.button');
+
+var PeopleConstuctor = function(name, age, state){
+  this.age = age;
+  this.name = name;
+  this.state = state;
+
+  this.printPerson = function(){
+    console.log(this.name + ", " + this.age + ", " + this.state);
+  }
+}
+
+var person1 = new PeopleConstuctor('John', 23, 'CA');
+var person2 = new PeopleConstuctor('Kim', 27, 'SC');
+
+button.addEventListener('click', function(){person1.printPerson()});
