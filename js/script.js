56a8155009e7e30591c12983bd252abfb3fc66de
Fisher <truefisherking@gmail.com>
2017-11-09 16:53:37 -0500

    Added peopleFactory class

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 var button = document.querySelector('.button');
+
+var peopleFactory = function(name, age, state){
+  var temp ={};
+  temp.age = age;
+  temp.name = name;
+  temp.state = state;
+
+  temp.printPerson = function(){
+    console.log(this.name + ", " + this.age + ", " + this.state);
+  }
+  return temp;
+}
+
+var person1 = peopleFactory('John', 23, 'CA');
+var person2 = peopleFactory('Kim', 27, 'SC');
+
+button.addEventListener('click', function(){person1.printPerson()});
