ea639d05beb49ef9f86abb0a677a401e72c5e2f0
Fisher <truefisherking@gmail.com>
2017-11-09 16:50:29 -0500

    Added PeopleProto class

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 var button = document.querySelector('.button');
+
+var PeopleProto = function(){
+}
+PeopleProto.prototype.age = 0;
+PeopleProto.prototype.name = "no name";
+PeopleProto.prototype.city = "no city";
+
+PeopleProto.prototype.printPerson = function(){
+  console.log(this.name + ", " + this.age + ", " + this.city);
+}
+var person5 = new PeopleProto();
+person5.name = "John";
+person5.age = 23;
+person5.city = "CA";
+
+button.addEventListener('click', function(){person5.printPerson()});
